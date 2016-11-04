ec7af5fbb4df3cf355df4e5d89cd6dd4f9c8a858
Omkar Ramtekkar <omkar.ramtekkar@citrix.com>
2016-11-04 14:57:15 +0530

    Added test app to test the pod

---
+++ TestFramework/TestFrameworkClass.h
@@ -0,0 +1,15 @@
+//
+//  TestFrameworkClass.h
+//  TestFramework
+//
+//  Created by Omkar on 11/4/16.
+//  Copyright © 2016 Omkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFrameworkClass : NSObject
+
++(void) test;
+
+@end
---
+++ TestFramework/TestFrameworkClass.m
@@ -0,0 +1,19 @@
+//
+//  TestFrameworkClass.m
+//  TestFramework
+//
+//  Created by Omkar on 11/4/16.
+//  Copyright © 2016 Omkar. All rights reserved.
+//
+
+#import "TestFrameworkClass.h"
+#import "TestFramework.h"
+
+@implementation TestFrameworkClass
+
++(void) test
+{
+    NSLog(@"Framework Testing - version - %f ", TestFrameworkVersionNumber);
+}
+
+@end
